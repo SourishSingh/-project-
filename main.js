ec77d0959287c282701aa4c51018a8f2337aa0b6
SourishSingh <93921293+SourishSingh@users.noreply.github.com>
2022-05-25 12:10:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+score1=0;
+score2=0;
+name1=;
+name2=;
+function update()
+{
+ name1=document.getElementById("i1").value;
+ name2=document.getElementById("i2").value;
+ console.log("registered"+name1+name2);
+};
+function names()
+{
+ document.getElementById("mk").innerHTML =name1+0;
+};
+ 
